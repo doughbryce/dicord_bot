182db15f95c852d3e22f9c6024326f494b77a1aa
doughbryce <31143323+doughbryce@users.noreply.github.com>
2022-07-10 18:01:36 -0600

    discord api

--- markov.py
+++ markov.py
@@ -2,6 +2,8 @@
 
 import sys
 from random import choice
+import os
+import discord
 
 
 def open_and_read_file(filenames):
@@ -64,3 +66,20 @@ text = open_and_read_file(filenames)
 
 # Get a Markov chain
 chains = make_chains(text)
+
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(f'Successfully connected! Logged in as {client.user}.')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return "hi bryce"
+
+
+print (client.run(os.environ['./secrets.sh']))
